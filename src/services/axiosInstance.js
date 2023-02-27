3aa8580e78361cab8117314b426d856c549a4ef6
TunarshBee <rafiubello4real@gmail.com>
2023-02-27 16:30:23 +0100

    edited axios instance

--- src/services/axiosInstance.js
+++ src/services/axiosInstance.js
@@ -1,4 +1,4 @@
 import axios from "axios";
 
-const baseUrl = process.env.REACT_APP_BASE_URL
+const baseUrl = process.env.REACT_APP_BASE_URL || "https://www.omdbapi.com"
 export const axiosInstance = axios.create({baseURL:baseUrl})
